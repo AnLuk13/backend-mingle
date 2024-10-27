bafa9ebf4d9891081ff079958a42456ba5621084
Antonio <antoniolupu72@gmail.com>
2024-10-27 17:38:15 +0200

    Added put and delete routes for users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -44,6 +44,39 @@ router.post("/", async (req, res) => {
   }
 });
 
+// put request by id
+router.put("/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findByIdAndUpdate(userId, req.body, {
+      new: true,
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// delete request by id
+router.delete("/:userId", async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    return res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
 //edit users wishlist
 router.put("/:userId/wishlist", async (req, res) => {
   const { userId } = req.params;
